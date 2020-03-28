20c5fcf833ea6aa7acca959c8ed3e7aed9ecc713
Diksha <dikshapurswani01@gmail.com>
2020-03-28 13:32:04 +0530

    Adding build

---
+++ build/precache-manifest.14a294b96fab630536e3beeb9d92d2f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76227c17c1661a8539cc76434d9b46e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "442738e10b36efbfdfe9",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "899813609262e9564bf9",
+    "url": "/static/css/main.312a3bf3.chunk.css"
+  },
+  {
+    "revision": "442738e10b36efbfdfe9",
+    "url": "/static/js/2.5446eb02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5446eb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899813609262e9564bf9",
+    "url": "/static/js/main.985eb6e0.chunk.js"
+  },
+  {
+    "revision": "6c06a79449601f60ff4c",
+    "url": "/static/js/runtime-main.6464c0d9.js"
+  }
+]);
